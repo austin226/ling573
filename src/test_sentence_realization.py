e82250fd2e57c1ea85bacfdbcb9dc3c917024c20
Austin Almond <aalmond@u.washington.edu>
2018-04-16 17:23:54 -0700

    Add a basic assertion for dumb realizer

--- src/test_sentence_realization.py
+++ src/test_sentence_realization.py
@@ -4,10 +4,17 @@ import unittest
 from sentence_realization import SentenceRealizer
 
 class SentenceRealizerTest(unittest.TestCase):
-    def test_dummy(self):
-        sentence_realizer = SentenceRealizer()
-        sentence_realizer.process(['sent1', 'sent2'])
-        print('ok')
+    def setUp(self):
+        self.sr = SentenceRealizer()
+
+    def test_basic_order(self):
+        sentences = [
+                'This is the first sentence.',
+                'This is the second sentence.',
+        ]
+        processed = self.sr.process(sentences)
+
+        self.assertEqual(sentences, processed)
 
 if __name__ == '__main__':
     unittest.main()
