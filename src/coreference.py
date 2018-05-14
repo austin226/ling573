605ad5b8811d2b8292787fd1d26bd7033ee69ae2
Austin Almond <aalmond@u.washington.edu>
2018-05-13 19:20:00 -0700

    Undo one-sent-per-run logic

--- src/coreference.py
+++ src/coreference.py
@@ -11,38 +11,35 @@ class CoreferenceResolver:
         #Set up the Stanford Toolkit
         nlp = StanfordCoreNLP('http://localhost:{}'.format(self.port))
 
-        resolved = []
+        #process all the sentences passed in
+        text = ' '.join(sentences)
 
-        for sentence in sentences:
-            resolved_sentence = sentence
-            #process all the sentences passed in
-            text = ' '.join(sentences)
+        output = nlp.annotate(text, properties={
+            'annotators': 'coref',
+            'pipelineLanguage': 'en',
+            'outputFormat': 'json'
+        })
 
-            output = nlp.annotate(text, properties={
-                'annotators': 'coref',
-                'pipelineLanguage': 'en',
-                'outputFormat': 'json'
-            })
+        #loop through coreference dictionary
+        for r, corefs in output['corefs'].items():
+            #find representative mention
+            for s in corefs:
+                if s['isRepresentativeMention']:
+                    replacementText = s['text']
+                    break
+            #replace representative mention
+            for s in corefs:
+                if not s['isRepresentativeMention']:
+                    sentIdx = s['sentNum'] - 1
+                    startIndex = s['startIndex']
+                    endIndex = s['endIndex']
 
-            #loop through coreference dictionary
-            for r, corefs in output['corefs'].items():
-                #find representative mention
-                for s in corefs:
-                    if s['isRepresentativeMention']:
-                        replacementText = s['text']
-                        break
-                #replace representative mention
-                for s in corefs:
-                    if not s['isRepresentativeMention']:
-                        startIndex = s['startIndex']
-                        endIndex = s['endIndex']
+                    tokens = output['sentences'][sentIdx]['tokens']
+                    editedSentence = [t['originalText'] for t in tokens]
+                    #handle multi-word replacements by removing additional words
+                    if startIndex != endIndex-1:
+                        editedSentence = editedSentence[0:startIndex] + editedSentence[endIndex-1:]
+                    print(sentences, len(sentences))
+                    sentences[sentIdx] = ' '.join(editedSentence)
 
-                        tokens = output['sentences'][0]['tokens']
-                        editedSentence = [t['originalText'] for t in tokens]
-                        #handle multi-word replacements by removing additional words
-                        if startIndex != endIndex-1:
-                            editedSentence = editedSentence[0:startIndex] + editedSentence[endIndex-1:]
-                        resolved_sentence = ' '.join(editedSentence)
-            resolved.append(resolved_sentence)
-
-        return resolved
+        return sentences
