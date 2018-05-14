2ecb6834d9039881f91e980308acc8359aed5acf
Austin Almond <aalmond@u.washington.edu>
2018-05-13 18:00:18 -0700

    Simplify venv setup

--- src/run.sh
+++ src/run.sh
@@ -4,11 +4,8 @@ export SERVER_LOG=$PROJECT_ROOT/var/log/StanfordCoreNLPServer.$(date +%s).log
 
 mkdir -p var/log
 
-rm -rf venv
-mkdir venv
-cd venv
-virtualenv -p python3 python3_virtualenv
-source ./python3_virtualenv/bin/activate
+virtualenv -p python3 venv
+source ./venv/bin/activate
 
 # Start the server
 $PROJECT_ROOT/src/server.sh &
