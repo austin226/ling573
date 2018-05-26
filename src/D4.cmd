40d14715ccf72f4bf9f55dcd3d0339b6a276af82
Austin Almond <aalmond@u.washington.edu>
2018-05-26 16:33:44 -0700

    Create D4.cmd

---
+++ src/D4.cmd
@@ -0,0 +1,8 @@
+executable = src/run.sh
+arguments = "/dropbox/17-18/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml outputs/D4"
+output = var/D4.out
+error = var/D4.error
+log = var/D4.log
+notification = complete
+request_memory = 4096
+Queue
