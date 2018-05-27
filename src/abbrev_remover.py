6d00661a622c958e69ca7e9ffbb41c7a2a890a70
corbettmoore <32318807+corbettmoore@users.noreply.github.com>
2018-05-27 15:31:48 -0700

    Add files via upload
    Added AbbrevRemover class to remove full stops from abbreviations and suspensions that contain them (e.g., e.g.).

---
+++ src/abbrev_remover.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import re
+
+class AbbrevRemover:
+    def remove_abbs(text: str):
+        abbrevs = ["a.m.", "acct.", "A.D.", "approx.", "Ave.", "B.A.", "B.C.", "Blvd.", "Bros.", "B.S.", "C.E.", "cf.",
+                   "cm.", "Dr.", "e.g.", "et al.", "etc.", "fig.", "ft.", "gal.", "I.D.", "i.e.", "in.", "km.", "L.A.",
+                   "M.A.", "M.B.A.", "mi.", "m.p.h.", "Mr.", "Mrs.", "M.S.", "Ms.", "Mt.", "N.Y.", "no.", "nos.",
+                   "p.", "Ph.D", "p.m.", "pp.", "qv.", "Rd.", "Rt.", "R.S.V.P.", "sq.", "St.", "U.S.", "U.S.A.",
+                   "V.I.P.", "vol."]
+
+        if "." not in text:  # Do not traverse text if it contains no full stops
+            return text
+        else:
+            for abb in abbrevs:
+                if abb not in text:  # Disregard abbreviations not present in text
+                    continue
+                else:
+                    abb_stripped = abb.replace('.', '')  # default is the abbreviation with capitals intact
+                    text = text.replace(abb, abb_stripped)  # assume abbrevs do not end sentences
+            match = re.search("([A-Z].)[ ]*([A-Z].) [A-Z]", text)
+            if match:
+                text = text.replace(match.group(1), match.group(1).replace('.', ''))
+                text = text.replace(match.group(2), match.group(2).replace('.', ''))
+            return text
