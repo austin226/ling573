b735da3368a2dfa3ad7219c30b0d6212fa016517
Austin Almond <aalmond@u.washington.edu>
2018-05-27 17:42:59 -0700

    Remove date lines and capitalize first word in sentence

--- src/sentence_realization.py
+++ src/sentence_realization.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import re
+
 class SentenceRealizer:
     '''
     The goal of this class is to "create a fluent, readable, compact output",
@@ -12,8 +14,18 @@ class SentenceRealizer:
         '''
 
         # Remove spaces before any non-word
-        sentences = [self.remove_extra_spaces(s) for s in sentences]
+        sentences = [self.compress_sentence(s) for s in sentences]
         return sentences
+
+    def compress_sentence(self, sentence):
+        # Use sentence trimming rules similar to CLASSY 2006
+        # Conroy, John & Schlesinger, Judith & O'leary, Dianne & Stewart, Jade. (2018). Back to basics: Classy 2006.
+        sentence = self.remove_extra_spaces(sentence)
+        sentence = self.remove_extraneous_words(sentence)
+        sentence = sentence.strip()
+        if len(sentence) > 0:
+            sentence = sentence[0].upper() + sentence[1:]
+        return sentence
 
     def remove_extra_spaces(self, sentence):
         tokens = sentence.split(' ')
@@ -28,3 +40,9 @@ class SentenceRealizer:
                 # Token is not a word, append to last token in output
                 output[-1] += t
         return ' '.join(output)
+
+    def remove_extraneous_words(self, sentence):
+        # Remove extraneous words that appear in a sentence, including date lines, editor's comments, and so on.
+        # Remove date lines
+        sentence = re.sub('^[A-Z ]+ _', '', sentence)
+        return sentence
